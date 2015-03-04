dfc80f9c2fcaa376333edd72097a72125a0e87f5
adamz01h <adamz01h@gmail.com>
2015-03-04 17:56:21 -0500

    Added 'loading' screen
    Added 'loading' screen

--- README.md
+++ README.md
@@ -13,3 +13,4 @@ Note:
 You may need to change the paths in eventcommand.sh to match your Apache working directory default is set to /var/www.
 All web files must be set to read & write for all users.
 Soundset.php can use PCM or Master , default is set for PCM
+
--- change.php
+++ change.php
@@ -10,10 +10,12 @@ function strbefore($string, $substring) {
    return(substr($string, 0, $pos));
 } 
 echo ($_POST['station']);
-echo exec('echo -n "s" >> /home/pi/.config/pianobar/ctl');
+echo exec('echo -n "s" >> ctl');
 $newnum = strbefore($_POST['station'], ')');
-echo exec ('echo -n ' . $newnum . ' >> /home/pi/.config/pianobar/ctl');
-echo exec ('echo "\r" >> /home/pi/.config/pianobar/ctl');
+echo exec ('echo -n ' . $newnum . ' >> ctl');
+echo exec ('echo "\r" >> ctl');
 echo $newnum;
+$load = file_get_contents('load');
+file_put_contents('songinfo.txt', $load);
 header("Location:index.php");
 ?>
