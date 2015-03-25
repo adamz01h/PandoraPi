f0025805b3feffb91e4d4aad5412be936b75d70c
adamz01h <adamz01h@gmail.com>
2015-03-25 13:51:43 -0400

    fixed install script
    added options for compile support

--- install.sh
+++ install.sh
@@ -1,10 +1,35 @@
 #!/bin/bash
 pwd=$(pwd)
-echo "Pandora User Name"
-read uname
-echo "Pandora Password"
-read upass
+
 sudo chmod -R 777 $pwd
+sudo apt-get install php5 libao-dev libgcrypt11-dev libgnutls-dev libfaad-dev libmad0-dev libjson0-dev make pkg-config
+
+	read -p "Download and compile FFmpeg?[y/n]" -n 1 -r
+	echo    # (optional) move to a new line
+	if [[ $REPLY =~ ^[Yy]$ ]] 
+	then
+	cd ~/
+	git clone https://github.com/FFmpeg/FFmpeg.git
+	cd FFmpeg
+	./configure --disable-yasm
+	make clean
+	make
+	sudo make install
+	cd $pwd
+	fi
+	
+	read -p "Download and compile Pianobar?[y/n]" -n 1 -r
+	echo    # (optional) move to a new line
+	if [[ $REPLY =~ ^[Yy]$ ]] 
+	then
+	cd ~/
+	git clone https://github.com/PromyLOPh/pianobar.git
+	cd pianobar
+	make clean
+	make
+	sudo make install
+	cd $pwd
+	fi
 
 if [ -d ~/.config/ ]
 then
@@ -26,16 +51,25 @@ then
 	echo    # (optional) move to a new line
 	if [[ $REPLY =~ ^[Yy]$ ]] 
 	then
+	echo "Pandora User Name"
+	read uname
+	echo "Pandora Password"
+	read upass
 	sudo chmod 666  ~/.config/pianobar/config
 	rm ~/.config/pianobar/config
+	touch  ~/.config/pianobar/config
+	sudo chmod 666 ~/.config/pianobar/config
+	echo "user = $uname" >> ~/.config/pianobar/config
+	echo "password = $upass" >> ~/.config/pianobar/config
 	fi
 else
 	touch  ~/.config/pianobar/config
 	sudo chmod 666 ~/.config/pianobar/config
+	echo "user = $uname" >> ~/.config/pianobar/config
+	echo "password = $upass" >> ~/.config/pianobar/config
 fi
 
-echo "user = $uname" >> ~/.config/pianobar/config
-echo "password = $upass" >> ~/.config/pianobar/config
+
 
 if  [  -a ctl  ]
 then
